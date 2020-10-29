59da314e69dbc77ec4a5fb89f717e8233f1909f3
wodniak98 <57682860+wodniak98@users.noreply.github.com>
2020-10-29 12:11:45 +0100

    skypt na player
    początek projektu, tylko skrypt na playera w posiadaniu projektu, zapewne nie działa

---
+++ Assets/Scrypty/Player.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [Range(1, 2)]
+    public int PlayerNumber = 1;
+    public float moveSpeed = 5f;
+    public bool canDropBombs = true;
+    public bool canMove = true;
+    private int bombs = 2; //2 bomby powinny starczyć,po eksplozji odzyskuje bombę
+
+    public GameObject bombPrefab;
+
+    private Rigidbody rigidBody;
+    private Transform myTransform;
+    private Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+        myTransform = transform;
+        animator = myTransform.Find("PlayerModel").GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateMovement();
+    }
+
+    private void UpdateMovement()
+    {
+        animator.SetBool("Walking", false);
+
+        if (!canMove)
+        {
+            return;
+        }
+
+        if (playerNumber == 1)
+        {
+            UpdatePlayer1Movement();
+        }
+        else
+        {
+            UpdatePlayer2Movement();
+        }
+    }
+    private void UpdatePlayer1Movement()
+    {
+        if (Input.GetKey(KeyCode.W))
+        { //Up movement
+            rigidBody.velocity = new Vector3(rigidBody.velocity.x, rigidBody.velocity.y, moveSpeed);
+            myTransform.rotation = Quaternion.Euler(0, 0, 0);
+            animator.SetBool("Walking", true);
+        }
+        if (Input.GetKey(KeyCode.A))
+        { //Left movement
+            rigidBody.velocity = new Vector3(-moveSpeed, rigidBody.velocity.y, rigidBody.velocity.z);
+            myTransform.rotation = Quaternion.Euler(0, 270, 0);
+            animator.SetBool("Walking", true);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        { //Down movement
+            rigidBody.velocity = new Vector3(rigidBody.velocity.x, rigidBody.velocity.y, -moveSpeed);
+            myTransform.rotation = Quaternion.Euler(0, 180, 0);
+            animator.SetBool("Walking", true);
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        { //Right movement
+            rigidBody.velocity = new Vector3(moveSpeed, rigidBody.velocity.y, rigidBody.velocity.z);
+            myTransform.rotation = Quaternion.Euler(0, 90, 0);
+            animator.SetBool("Walking", true);
+        }
+        if (canDropBombs && (Input.GetKeyDown(KeyCode.KeypadEnter) || Input.GetKeyDown(KeyCode.Return)))
+        { //Drop Bomb. For Player 2's bombs, allow both the numeric enter as the return key or players 
+            //without a numpad will be unable to drop bombs
+            DropBomb();
+        }
+    }
+    private void DropBomb()
+    {
+        if (bombPrefab)
+        {
+
+        }
+    }
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Explosion"))
+        {
+            Debug.Log("P" + playerNumber + " hit by explosion!");
+        }
+    }
+}
